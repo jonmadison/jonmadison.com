85c5e8cdf1dfa79a76cbf67a8ae651103eebbb08
jon madison <jon@tfftech.com>
2016-02-16 19:22:58 -0800

    lean towards webpack for css loading, add bg color to page

--- app/App.js
+++ app/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
-import routes from './config/routes';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router";
+import routes from "./config/routes";
+require("style!css!../node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("style!css!../public/css/style.css");
+require("style!css!../public/css/animation.css");
 ReactDOM.render(
     <Router>{routes}</Router>,
     document.getElementById("app")
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 module.exports = {
-  entry: "./app/App.js",
-  output: {
-    filename: "public/bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+    entry: "./app/App.js",
+    output: {
+        filename: "public/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel",
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            },
+            {
+                test: /\.(eot|woff).*$/,
+                loader: "file-loader"
+            }
+        ]
+    }
 };
