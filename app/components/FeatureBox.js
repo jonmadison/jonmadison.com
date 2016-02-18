5e7fc617c35abda9b3dfdd5cc8b90986d35d8471
jon madison <jon@tfftech.com>
2016-02-17 21:24:30 -0800

    remove link

--- app/components/FeatureBox.js
+++ app/components/FeatureBox.js
@@ -7,25 +7,23 @@ require("style!css!../../public/css/FeatureBox.css");
 let FeatureBox = React.createClass({
     render: function() {
         return (
-                <a href="./#/learning">
-                    <ReactCSSTransitionGroup
-                    transitionName="feature-box-load"
-                    transitionAppear={true}
-                    transitionAppearTimeout={this.props.appearTimeout || 300}
-                    transitionEnterTimeout={300}
-                    transitionLeaveTimeout={300}>
-                        <div key={UniqueId()} className="feature-box" id={this.props.name}>
-                            <h1>{this.props.name}</h1>
-                            <ul>
-                                {
-                                    this.props.items.map( (item) => {
-                                        return <li key={UniqueId()}>{item}</li>;
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </ReactCSSTransitionGroup>
-                </a>
+            <ReactCSSTransitionGroup
+            transitionName="feature-box-load"
+            transitionAppear={true}
+            transitionAppearTimeout={this.props.appearTimeout || 300}
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}>
+                <div key={UniqueId()} className="feature-box" id={this.props.name}>
+                    <h1>{this.props.name}</h1>
+                    <ul>
+                        {
+                            this.props.items.map( (item) => {
+                                return <li key={UniqueId()}>{item}</li>;
+                            })
+                        }
+                    </ul>
+                </div>
+            </ReactCSSTransitionGroup>
         );
     }
 });
